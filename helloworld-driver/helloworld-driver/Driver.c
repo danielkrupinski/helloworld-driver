6000208ccf82f4e360e4b19fbab976ae3f175e20
Daniel Krupiński <danielkrupinski@outlook.com>
2019-09-12 18:21:50 +0200

    Mark unreferenced parameters

--- helloworld-driver/helloworld-driver/Driver.c
+++ helloworld-driver/helloworld-driver/Driver.c
@@ -5,6 +5,9 @@ DRIVER_INITIALIZE DriverEntry;
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
+    UNREFERENCED_PARAMETER(DriverObject);
+    UNREFERENCED_PARAMETER(RegistryPath);
+
     DbgPrint("Hello from driver!\n");
     return STATUS_SUCCESS;
 }
